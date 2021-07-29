b8085784e69451b59c1728c2fec1cc00a29bb218
Marcos Paz <31805148+marcosvspaz@users.noreply.github.com>
2021-07-29 15:52:15 -0300

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header() {
     return <Container>
 
     
-        <Title>Poliglotism</Title>
+        <Title>The Polyglots</Title>
         <MenuContainer>
         <Link href='/'> 
         <Menu>DuoLingu</Menu>
@@ -15,39 +15,13 @@ function Header() {
         <Link href='/https://www.internetpolyglot.com/'> 
         <Menu>Internet Poliglot</Menu>
         </Link>
-        <Link href='/apoiase'> 
-        <Menu>English</Menu>
+        <Link href='https://www.ankiweb.net/'> 
+        <Menu>Anki</Menu>
         </Link>
         <Link href='/sobre'> 
         <Menu>Japanese</Menu>
         </Link>
-        <Link href='/sobre'> 
-        <Menu>Italian</Menu>
-        </Link>
-        <Link href='/sobre'> 
-        <Menu>Corean</Menu>
-        </Link>
-        <Link href='/sobre'> 
-        <Menu>Spanish</Menu>
-        </Link>
-        <Link href='/sobre'> 
-        <Menu>Chinese</Menu>
-        </Link>
-        <Link href='/sobre'> 
-        <Menu>Germany</Menu>
-        </Link>
-        <Link href='/sobre'> 
-        <Menu>French</Menu>
-        </Link>
-        <Link href='/sobre'> 
-        <Menu>Russian</Menu>
-        </Link>
-        <Link href='/sobre'> 
-        <Menu>Arabic</Menu>
-        </Link>
-        <Link href='/sobre'> 
-        <Menu>Swedish</Menu>
-        </Link>
+    
 </MenuContainer>
 
 
